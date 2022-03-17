091603d7aa6a0b4a06d87d6e3e6e26f2456d5a8d
Roga <ryanroga@gmail.com>
2022-03-17 15:14:13 -0700

    Update global fonts

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,10 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inter", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
