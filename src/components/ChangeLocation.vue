e7b39d9e2da52416564f6b7b152752dd8ab5315e
Roga <ryanroga@gmail.com>
2022-03-17 15:23:01 -0700

    Create ChangeLocation.vue

---
+++ src/components/ChangeLocation.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+const location = ref("");
+</script>
+
+<template>
+  <div class="container">
+    <h2>Change Location</h2>
+    <input type="text" id="location" v-model="location" />
+    <button type="button" @click="changeLocation">Change Location</button>
+  </div>
+</template>
+
+<style scoped>
+/*  */
+</style>
