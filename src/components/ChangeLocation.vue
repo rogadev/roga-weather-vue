2866edd0e56d438bb33c6a097d979698254515f3
Roga <ryanroga@gmail.com>
2022-03-18 21:10:56 -0700

    Update ChangeLocation.vue
    Change from using local ref to store variable

--- src/components/ChangeLocation.vue
+++ src/components/ChangeLocation.vue
@@ -1,13 +1,19 @@
 <script setup>
 import { ref } from "vue";
+import { store } from "../store";
 
-const location = ref("");
+const location = ref(store.state.location);
+
+const changeLocation = () => {
+  store.state.location = location.value;
+  location.value = "";
+};
 </script>
 
 <template>
   <div class="container">
     <h2>Change Location</h2>
-    <input type="text" id="location" v-model="location" />
+    <input type="text" id="location" name="location" v-model="location" />
     <button type="button" @click="changeLocation">Change Location</button>
   </div>
 </template>
