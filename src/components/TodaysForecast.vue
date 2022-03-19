9b621bae53dcc584e60a71d9723076f383b5fc9e
Roga <ryanroga@gmail.com>
2022-03-19 15:46:21 -0700

    Update TodaysForecast.vue
    Attempting to add props, instead of looking for reactive state change. Was not getting desired effect, previously.

--- src/components/TodaysForecast.vue
+++ src/components/TodaysForecast.vue
@@ -1,14 +1,42 @@
 <script setup>
-import { ref } from "vue";
-import { store } from "../store";
+import { defineProps } from "vue";
 
-const conditions = ref(store.state.todaysForecast);
+defineProps({
+  currentTemp: {
+    type: String,
+    default: "",
+  },
+  precip: {
+    type: String,
+    default: "",
+  },
+  humidity: {
+    type: String,
+    default: "",
+  },
+  wind: {
+    type: Number,
+    default: 0,
+  },
+  icon: {
+    type: String,
+    default: "",
+  },
+  comment: {
+    type: String,
+    default: "Shitty weather",
+  },
+});
 </script>
 
 <template>
   <section>
     <h1>Today's Forecast</h1>
-    <p>{{ conditions }}</p>
+    <p>{{ comment }} today</p>
+    <p>Currently {{ currentTemp }} degrees C</p>
+    <p>Chance of rain: {{ precip }}%</p>
+    <p>Humidity: {{ humidity }}%</p>
+    <img :src="iconURL" :alt="comment" />
   </section>
 </template>
 
