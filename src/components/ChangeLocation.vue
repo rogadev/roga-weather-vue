de92011575ba15745a5a01ba9d5ce45117bd2235
Roga <ryanroga@gmail.com>
2022-03-19 13:19:51 -0700

    Adds Style to ChangeLocation.vue
    Changing location still does not yet effect the page. Cleaned up and added styling.

--- src/components/ChangeLocation.vue
+++ src/components/ChangeLocation.vue
@@ -5,16 +5,30 @@ import { store } from "../store";
 const location = ref(store.state.location);
 
 const changeLocation = () => {
-  store.state.location = location.value;
-  location.value = "";
+  //todo
 };
 </script>
 
 <template>
-  <div class="container">
-    <h2>Change Location</h2>
-    <input type="text" id="location" name="location" v-model="location" />
-    <button type="button" @click="changeLocation">Change Location</button>
+  <div class="container mb-2 border py-2 rounded bg-slate-50 shadow-md">
+    <h2 class="text-2xl mb-2">Change Location</h2>
+    <div class="py-1">
+      <input
+        class="border rounded-sm px-2 py-1 mr-4"
+        type="text"
+        id="location"
+        name="location"
+        v-model="location"
+        @keyup.enter="changeLocation"
+      />
+      <button
+        type="button"
+        @click="changeLocation"
+        class="border rounded-md bg-blue-100 px-2 py-1 shadow"
+      >
+        Change Location
+      </button>
+    </div>
   </div>
 </template>
 
