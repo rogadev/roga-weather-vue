22cbeab84dc46364ff08bd52a5d20075e90392b4
Roga <ryanroga@gmail.com>
2022-03-19 12:19:57 -0700

    Styling App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,26 @@ import Loading from "./components/Loading.vue";
 </script>
 
 <template>
-  <div>
-    <header>
-      <h1>Current Local Forecast</h1>
-      <small
-        >Developed by Ryan Roga |
-        <a href="https://www.roga.dev">Roga.dev</a></small
-      >
-    </header>
-    <main>
-      <ChangeLocation />
-      <Suspense>
-        <template #default>
-          <Weather />
-        </template>
-        <template #fallback>
-          <Loading />
-        </template>
-      </Suspense>
-    </main>
-  </div>
+  <header class="mb-3">
+    <h1 class="text-4xl font-bold py-4">Current Local Forecast</h1>
+    <small class="font-bold"
+      >Developed by Ryan Roga |
+      <a href="https://www.roga.dev" class="text-blue-500 font-bold"
+        >Roga.dev</a
+      ></small
+    >
+  </header>
+  <main class="mx-auto w-4/5 p-2">
+    <ChangeLocation />
+    <Suspense>
+      <template #default>
+        <Weather />
+      </template>
+      <template #fallback>
+        <Loading />
+      </template>
+    </Suspense>
+  </main>
 </template>
 
 <style>
