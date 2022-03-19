1fd737faf2ed1977ad7adddaf3d643ddf49160f0
Roga <ryanroga@gmail.com>
2022-03-19 13:54:30 -0700

    Update fetchGeo.js

--- src/utils/fetchGeo.js
+++ src/utils/fetchGeo.js
@@ -8,10 +8,13 @@ import { store } from '../store';
 const GEO_IP_LOOKUP_API_URL = 'https://json.geoiplookup.io/';
 
 /* Get geo location based on ip address */
-console.log('Fetching location data...');
-const getLocation = useFetch(GEO_IP_LOOKUP_API_URL, { skip: true });
-await getLocation.fetch();
-store.state.location = `${getLocation.response.value.city}, ${getLocation.response.value.region}`;
-const getSlug = useSlug(store.state.location);
-store.state.locationSlug = getSlug.slug;
-console.log('Location data fetched!', store.state.location);
+
+export default async () => {
+	console.log('Fetching location data...');
+	const getLocation = useFetch(GEO_IP_LOOKUP_API_URL, { skip: true });
+	await getLocation.fetch();
+	store.state.location = `${getLocation.response.value.city}, ${getLocation.response.value.region}`;
+	const getSlug = useSlug(store.state.location);
+	store.state.locationSlug = getSlug.slug;
+	console.log('Location data fetched!', store.state.location);
+};
