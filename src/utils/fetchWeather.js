f29cbf51bfcb5ff43766c9d83eafb6004391ff8e
vonroga <ryanroga@gmail.com>
2022-03-28 22:26:23 -0700

    Update fetchWeather.js
    Created a metaweather proxy server but still having CORS issues.

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,34 +1,41 @@
-import { ref, reactive } from 'vue';
+import { ref, reactive } from 'vue'
 
-import { useFetch } from '../composables/useFetch';
-import { store } from '../store';
+import { useFetch } from '../composables/useFetch'
+import { store } from '../store'
 
 const getKey = () => {
-	const currentHour = Math.floor(new Date().getTime() / 3600000);
-	return `${currentHour}-${store.state.locationSlug}`;
-};
+  const currentHour = Math.floor(new Date().getTime() / 3600000)
+  return `${currentHour}-${store.state.locationSlug}`
+}
 
 const fetchWeather = async () => {
-	const loading = ref({});
+  const loading = ref({})
 
-	store.state.loading = true;
-	// Log
-	console.log('Fetching weather...');
+  store.state.loading = true
+  const METAWEATHER_PROXY = 'https://metaweather-proxy.herokuapp.com/?location='
 
-	// Fetch current conditions
-	const WEATHER_DBI_API_URL = 'https://weatherdbi.herokuapp.com/data/weather/';
-	const getWeather = useFetch(WEATHER_DBI_API_URL + store.state.locationSlug, {
-		skip: true,
-		key: getKey(),
-	});
-	await getWeather.fetch();
+  // Log
+  console.log('Fetching weather...')
 
-	// Update State
-	store.state.loading = loading.value = false;
-	store.state.weather = reactive(getWeather.data.value);
+  // Fetch current conditions
+  const getWeather = useFetch(METAWEATHER_PROXY + store.state.locationSlug, {
+    skip: true,
+    key: getKey(),
+  })
+  try {
+    await getWeather.fetch()
+  } catch (e) {
+    console.log(e)
+  } finally {
+    console.log('Fetched weather', getWeather.data)
+  }
 
-	// Log
-	console.log('Weather fetch complete.');
-};
+  // Update State
+  store.state.loading = loading.value = false
+  store.state.weather = reactive(getWeather.data.value)
 
-export { fetchWeather };
+  // Log
+  console.log('Weather fetch complete.')
+}
+
+export { fetchWeather }
